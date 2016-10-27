80fafb24b3cf4575e5f631673208c61fd4da0952
Owen Moss <ommoss63@gmail.com>
2016-10-27 23:26:31 +0000

    option now slides over text in fillings menu

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,14 +1,29 @@
-$(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });;
+// $(() => {
+//   $.ajax({
+//     method: "GET",
+//     url: "/api/users"
+//   }).done((users) => {
+//     for(user of users) {
+//       $("<div>").text(user.name).appendTo($("body"));
+//     }
+//   });
+// });
+
+// drop down bars
+$(document).ready(function(){
+  $('.fillings').hide();
+  $('.types').hide();
 });
 
 $(".start").click(function(){
   $(".fillings").slideToggle("fast","linear");
+});
+
+$("#chicken").click(function(){
+  $(".chickenTypes").slideToggle("fast","linear");
+});
+
+$(".meatMenu").click(function(){
+  $(this).siblings().toggle();
+  $(this).toggleClass( "width" )
 });
