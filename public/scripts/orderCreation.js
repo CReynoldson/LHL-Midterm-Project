07bded7cbed01d2fc25cdccc0ccebcb5992f71b1
Courtney Reynoldson <ctreynoldson@gmail.com>
2016-10-29 01:14:57 +0000

    fully functioning order item created

--- public/scripts/orderCreation.js
+++ public/scripts/orderCreation.js
@@ -1,35 +1,110 @@
-//select active buttons and make them into an object
 
+//hold multiple items in a given order
 var totalOrder = [];
 
+//take active choices and turn them into an orderItem object
 $("#addToOrder").on("click", function(){
-
+var typesOfProtein = ["#chicken","#beef","#veg","#fish"];
 var possibleOrderFillings = ["#crispy", "#grilled", "#ground", "#steak", "#halibut", "#cod", "#tofu", "#rice"]
 var addOns = ["#sourCream", "#guac", "#salsa"];
 
   var orderItem = {
     type: "",
+    filling: "",
     sauce: "",
-    extras: []
+    extras: [],
+    spiciness: function (num){
+      var text = ""
+      switch(num){
+        case 1:
+        case 2:
+        text = "Spiciness: Mild";
+        break;
+        case 3:
+        case 4:
+        text = "Spiciness: Moderate";
+        break;
+        case 5:
+        case 6:
+        text = "Spiciness: Spicy";
+        break;
+        case 7:
+        text = "Spiciness: Pepper Spray";
+        break;
+        case 8:
+        text = "Spiciness: The Dark Lord Satan Himself";
+        break;
+      }
+      return text;
+    },
+    capitalize: function (string) {
+      return string.charAt(0).toUpperCase() + string.slice(1);
+    },
+    stringify: function (){
+      var string = "";
+      if (this.type === "chicken"){
+        string += this.capitalize(this.filling) + " Chicken Burrito ";
+        if (this.extras.length === 0){
+          string += this.spiciness(Number(this.sauce));
+        } else {
+          string += "with ";
+          for (var i = 0; i < this.extras.length; i++){
+            string += this.capitalize(this.extras[i]) + ", ";
+          }
+          string += this.spiciness(Number(this.sauce));
+        }
+        return string;
+      } else {
+        string += this.capitalize(this.filling) + " Burrito ";
+        if (this.extras.length === 0){
+          string += this.spiciness(Number(this.sauce));
+        } else {
+          string += "with ";
+          for (var i = 0; i < this.extras.length; i++){
+            string += this.capitalize(this.extras[i]) + ", ";
+          }
+          string += this.spiciness(Number(this.sauce));
+        }
+        return string;
+      }
+    }
   };
+
+  typesOfProtein.forEach(function (elm){
+    if ($(elm).is(":visible")){
+      orderItem.type = elm.substring(1);
+    }
+  });
+  console.log(orderItem.type);
+
+  possibleOrderFillings.forEach(function (elm){
+
+    if ($(elm).is(":visible")){
+      orderItem.filling = elm.substring(1);
+      if (orderItem.filling === "ground"){
+        orderItem.filling = "Ground Beef";
+      }
+    }
+  });
 
   orderItem.sauce = $("#sauce").val();
 
-  possibleOrderFillings.forEach(function (elm){
-    if ($(elm).is(":visible")){
-      orderItem.type = elm;
-    }
-  });
   addOns.forEach(function (elm){
-    if ($(elm).hasClass("active")){
-      orderItem.extras.push(elm);
+    if ($(elm).hasClass("active") && ($(elm).text().trim() == "Sour Cream")){
+      orderItem.extras.push("Sour Cream");
+    } else if ($(elm).hasClass("active")){
+      orderItem.extras.push(elm.substring(1));
     }
   });
+
+
+
   totalOrder.push(orderItem);
- console.log(orderItem);
- console.log(totalOrder);
+  console.log("THE ONE UNDERNEATH ME IS WHAT YOU'RE LOOKING FOR");
+  console.log(orderItem.stringify());
 
 
+  //hide everything so the customer can start a new order
   $(".fillings").slideToggle("fast","linear");
   $('.chickenTypes').hide();
   $('.fishTypes').hide();
@@ -43,5 +118,57 @@ var addOns = ["#sourCream", "#guac", "#salsa"];
   $(".meatMenu").removeClass("hide");
   $(".meatMenu").removeClass("active");
   $(".third").removeClass("active");
+  $(".chickenP").addClass("hide");
+  $(".beefP").addClass("hide");
+  $(".vegP").addClass("hide");
+  $(".fishP").addClass("hide");
+  $(".girlledP").addClass("hide");
+  $(".crispyP").addClass("hide");
+  $(".steakP").addClass("hide");
+  $(".groundP").addClass("hide");
+  $(".tofuP").addClass("hide");
+  $(".riceP").addClass("hide");
+  $(".halibutP").addClass("hide");
+  $(".codP").addClass("hide");
+  $(".heat").addClass("hide");
+
+  console.log("HI!");
+totalOrder.forEach(function (elm){
+  console.log(elm.filling);
 });
+console.log("Bye!");
+
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
