15a01ce19e5b3032a3421372c1634121910659fc
Owen Moss <ommoss63@gmail.com>
2016-10-30 00:34:38 +0000

    added Twilio function

--- server.js
+++ server.js
@@ -16,6 +16,10 @@ const knexLogger  = require('knex-logger');
 
 // Seperated Routes for each Resource
 const usersRoutes = require("./routes/users");
+var accountSid = 'ACc3d9540af089cd45dc236d18dc178043';
+var authToken = 'b0f6a5c2594c547905fe08ad215b0967';
+var twilio = require('twilio');
+var client = new twilio.RestClient(accountSid, authToken);
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
@@ -66,6 +70,16 @@ app.get("/current-orders", (req, res) => {
 app.get("/orders-in-progress", (req, res) => {
   res.render("restaurant_orders");
 });
+app.post("/sendOrder", (req, res) => {
+    client.messages.create({
+        to: "+12505889033",
+        from: "+17784001527",
+        body: "Your order will be ready in 5 minutes!"
+    }, function(err, message) {
+        console.log(message.sid);
+    });
+  res.redirect("/restaurants/id")
+})
 
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
